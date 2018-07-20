f0ca89a83923d7d5780f91b8fcf2fa68c123c592
fagnersutel <fagnersutel@hotmail.com>
2018-07-20 15:42:59 -0300

    Add files via upload

--- Distribuição_conjunta_de_probabilidades.R
+++ Distribuição_conjunta_de_probabilidades.R
@@ -40,8 +40,12 @@ bmax
 freq_table <- ddply(rolls1, ~a, summarize,
                     "2"=sum(b==2), "3"= sum(b==3),
                     "4" = sum(b==4), "5"=sum(b==5), "6"=sum(b==6))
+
+ddply(rolls1, ~a, summarize,
+      "2"=sum(b==2), "3"= sum(b==3),
+      "4" = sum(b==4), "5"=sum(b==5), "6"=sum(b==6))
 row.names(freq_table) <- c(amin: amax)
-row.names(freq_table) <- paste0(' ',0:2)
+#row.names(freq_table) <- paste0(' ',0:2)
 
 prob_table <- freq_table[,-1]/10
 
@@ -49,25 +53,3 @@ prob_table
 
 View(prob_table)
 
-a = c(1,2,3,1,3,2,3,1,2,3)
-b = c(2,1,3,1,3,3,2,1,2,3)
-a
-b
-ab = cbind(a, b)
-ab = as.data.frame(ab)
-ab
-rolls1 <- ab
-amin= min(ab$a)
-amin
-amax = max(ab$a)
-amax
-bmin = min(ab$b)
-bmin
-bmax = max(ab$b)
-bmax
-freq_table <- ddply(rolls1, ~a, summarize,
-                    "1"=sum(b==1),"2"=sum(b==2), "3"= sum(b==3))
-row.names(freq_table) <- c(amin: amax)
-
-prob_table <- freq_table[,-1]/10
-prob_table
